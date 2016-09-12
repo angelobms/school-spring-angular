741baefbeeab566b7000a137d7dcd28746203ff7
Angelo Brandão <angelobms@gmail.com>
2016-09-12 12:20:06 -0300

    Commit initial.

---
+++ src/main/java/br/com/bmsti/school/SchoolSpringAngularApplication.java
@@ -0,0 +1,12 @@
+package br.com.bmsti.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolSpringAngularApplication.class, args);
+	}
+}
